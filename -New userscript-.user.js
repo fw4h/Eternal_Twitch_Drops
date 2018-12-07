d778dc10100a0eec0b763cbdf43c67f5370e06db
Da Wang <fw4hfw4h@gmail.com>
2018-12-06 21:49:55 -0600

    If drops are not enabled, script will refresh

--- -New userscript-.user.js
+++ -New userscript-.user.js
@@ -10,82 +10,105 @@
 
 
 var DIRECTORY_URL = 'https://www.twitch.tv/directory/game/Eternal';
- 
-(function() {
+
+(function () {
     'use strict';
     var nbReloads = 0;
-   
+
     function stuff() {
-        var currentGame = document.getElementsByClassName('drops-campaign-details__drops-success tw-strong')[0].textContent;
-       
-         if (window.location.href == DIRECTORY_URL) {
+
+
+        if (window.location.href == DIRECTORY_URL) {
             // choose a streamer
             var streams = document.getElementsByClassName('tw-mg-b-2 tw-relative');
             var validStreams = [];
-            
+
             console.log(streams.length);
             // for(var i=0; i < streams.length; i++) {
             //     // remove vodcasts and low viewer counts (might not have quality options)
             //     var isVodCast = streams[i].getElementsByClassName('is-watch-party').length;
             //     var viewerCount = streams[i].getElementsByClassName('card__info')[0].getElementsByClassName('ember-view')[0].innerHTML.split(' ')[0];
-               
+
             //     if(!isVodCast && viewerCount > 100) {
             //         validStreams.push(streams[i].getElementsByTagName('a')[0].href);
             //     }
             // }
-             
-            if(streams.length) {
+
+            if (streams.length) {
                 // select a random stream
-                window.location = streams[Math.floor(Math.random() * streams.length)];
-            } else {
-                // we filtered everything out... Go to #2 directly (to give a chance to get them to #1 and shuffle the streamer order a bit!)
-                //window.location = streams[Math.min(1, streams.length-1)].getElementsByTagName('a')[0].href;
-            }
-        } else if (!(currentGame === "Drops enabled!")) {
-            // get back to ESL!
-            //alert(currentGame);
-            window.location = DIRECTORY_URL;
-        } else {
-            // re-evaluate every now and then and reload every few cycles incase something gets stuck
-            nbReloads++;
-            if(nbReloads >= 6) {
-                location.reload();
-            } else {
-                setTimeout(stuff, 1000*60*5);
+                //window.location = streams[Math.floor(Math.random() * streams.length)];
+                window.location = streams[Math.floor(Math.random() * streams.length)].getElementsByTagName('a')[0].href;
             }
         }
+        // check the existence of drops 
+        else if (typeof document.getElementsByClassName('drops-campaign-details__drops-success tw-strong')[0] != "undefined") {
+            var currentGame = document.getElementsByClassName('drops-campaign-details__drops-success tw-strong')[0].textContent;
+            if (!(currentGame === "Drops enabled!")) {
+                // get back to ESL!
+                //alert(currentGame);
+                window.location = DIRECTORY_URL;
+            } else {
+                // re-evaluate every now and then and reload every few cycles incase something gets stuck
+                nbReloads++;
+                if (nbReloads >= 6) {
+                    location.reload();
+                } else {
+                    setTimeout(stuff, 1000 * 60 * 5);
+                } 
+                // var mounsedownEvent = new MouseEvent('mousedown');
+                // var mounseupEvent = new MouseEvent('mouseup');
+                // setInterval(function () {
+                //     if (typeof document.getElementById('bronzeIdle-output') != "undefined") {
+                //         var activeDropBronze = document.getElementById('bronzeIdle-output');
+                //         var testdwang62 = document.getElementsByClassName('tw-svg__asset tw-svg__asset--more tw-svg__asset--inherit')[0];
+                        
+                //         testdwang62.dispatchEvent(mounsedownEvent);
+                //         testdwang62.dispatchEvent(mounseupEvent);
+                //         //activeDropBronze.click();
+                //         if (typeof document.getElementById('silverIdle-output') != "undefined") {
+                //             //var activeDropSilver = document.getElementById('silverIdle-output');
+                //             //activeDropSilver.click();
+                //         }
+                //     }
+                // }, 200);
+            }
+        }
+        else {
+            window.location = DIRECTORY_URL;
+        }
+
     }
-   
-    setTimeout(stuff, 1000*10);
 
-// (function () {
-//     'use strict';
-//     document.addEventListener("DOMContentLoaded", function() {
-//         var b = document.getElementById('tw-svg__asset tw-svg__asset--more tw-svg__asset--inherit');
-//         b.addEventListener('click', test, false);
-//     });
+    setTimeout(stuff, 1000 * 10);
 
-//     var activeDropBronze = document.getElementById('bronzeIdle-output');
-//     setInterval(function () {
-//         //activeDropBronze.click()
-//     }, 200);
+    // (function () {
+    //     'use strict';
+    //     document.addEventListener("DOMContentLoaded", function() {
+    //         var b = document.getElementById('tw-svg__asset tw-svg__asset--more tw-svg__asset--inherit');
+    //         b.addEventListener('click', test, false);
+    //     });
 
-//     var activeDropSilver = document.getElementById('silverIdle-output');
-//     setInterval(function () {
-//         //activeDropSilver.click()
-//     }, 200);
+    //     var activeDropBronze = document.getElementById('bronzeIdle-output');
+    //     setInterval(function () {
+    //         //activeDropBronze.click()
+    //     }, 200);
 
-//     var testdwang62 = document.querySelector('tw-svg__asset tw-svg__asset--more tw-svg__asset--inherit');
-//     var mounsedownEvent = new MouseEvent('mousedown');
-//     var mounseupEvent = new MouseEvent('mouseup');
+    //     var activeDropSilver = document.getElementById('silverIdle-output');
+    //     setInterval(function () {
+    //         //activeDropSilver.click()
+    //     }, 200);
 
-//     setInterval(function () {
-//         testdwang62.InvokeMember("click");
-//         // test.dispatchEvent(mounsedownEvent);
-//         // test.dispatchEvent(mounseupEvent);
-//         // test.trigger('click')
-//         //test.click()
-//     }, 200);
+    //     var testdwang62 = document.querySelector('tw-svg__asset tw-svg__asset--more tw-svg__asset--inherit');
+    //     var mounsedownEvent = new MouseEvent('mousedown');
+    //     var mounseupEvent = new MouseEvent('mouseup');
+
+    //     setInterval(function () {
+    //         testdwang62.InvokeMember("click");
+    //         // test.dispatchEvent(mounsedownEvent);
+    //         // test.dispatchEvent(mounseupEvent);
+    //         // test.trigger('click')
+    //         //test.click()
+    //     }, 200);
 
     // $('tw-svg__asset tw-svg__asset--more tw-svg__asset--inherit').trigger('click');
     // $(document).ready(function () {
