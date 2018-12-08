f8ab617f2256486ab40b96df3447c9079e3c7a4c
Da Wang <fw4hfw4h@gmail.com>
2018-12-07 23:03:57 -0600

    Refined Sikuli scripts

--- Eternal_Twitch_Drops.sikuli/Eternal_Twitch_Drops.py
+++ Eternal_Twitch_Drops.sikuli/Eternal_Twitch_Drops.py
@@ -1,10 +1,13 @@
+setThrowException(False)
 while True:
     if exists("bronzedrop.PNG"):
         click("bronzedrop.PNG")
+    if exists(Pattern("silverdrop.PNG").similar(0.50)):
+        click(Pattern("silverdrop-1.PNG").similar(0.50))
         continue
     else:
         continue
 
-      
+
 
 
