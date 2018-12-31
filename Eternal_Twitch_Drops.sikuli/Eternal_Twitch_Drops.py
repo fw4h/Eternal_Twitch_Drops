5366bc21bc30d40de5e857422ca58a3f53767d0d
Da Wang <fw4hfw4h@gmail.com>
2018-12-31 11:42:29 -0800

    Fixed the clicking issues

--- Eternal_Twitch_Drops.sikuli/Eternal_Twitch_Drops.py
+++ Eternal_Twitch_Drops.sikuli/Eternal_Twitch_Drops.py
@@ -1,14 +1,23 @@
 setThrowException(False)
 while True:
     if exists("bronzedrop.PNG"):
-        click("bronzedrop.PNG")
+        hover("bronzedrop.PNG")
+        wait(2)
+        doubleClick()
     if exists(Pattern("silverdrop.PNG").similar(0.50)):
-        click(Pattern("silverdrop-1.PNG").similar(0.50))
+        hover(Pattern("silverdrop-1.PNG").similar(0.50))
+        wait(2)
+        doubleClick()
     if not Region(0,73,59,58).exists("1544480537332.png"):
-        wait(30)
+        wait(3)
         if not Region(0,73,59,58).exists("1544480537332.png"):
+            hover(Location(51, 16))
+            wait(2)
             click(Location(51, 16))
-            click("1544364981762.png")
+            hover("1544364981762.png")
+            wait(2)
+            click()
+            
 
 
 
